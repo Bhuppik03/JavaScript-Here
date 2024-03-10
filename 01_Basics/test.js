ba3d4d6196b43bb6fad1afefdc292375a2c464f4
Bhupendra Kumar <149204876+Bhuppik03@users.noreply.github.com>
2024-03-10 08:58:40 +0000

    Basics Introduction JS

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,50 @@
 console.log("Bhuppi Here")
+/*
+What is JavaScript?
+JS is a programming language.We use it to give instructions to the compuetr.
+
+input(code) ----> Computer ----> output
+1. Instruction give the (in the form of code) and computer give the output
+
+2. Write the code of javascript
+
+3. JavaScript change the content of HTML
+
+4. JavaScript allows you to make web pages more interactive and accessing and modifying the content and markup used in a web page while it is being viewed in the broswer.
+
+5. You can use JS to add attributes, element and text to the page, or remove them.for exmaple
+• Add a paragraph of text after the 
+first <hl> element 
+• Change the value of c 1 ass 
+attributes to trigger new CSS rules 
+for those elements 
+• Change the size or position of an 
+<i mg> element 
+
+6.PROGRAM RULES : You can specify a set of steps for  the browser to follow (like a recipe), which allows it to access or change the  content of a page. For example:
+• A gallery script could check which image a user clicked on and display a larger version of that image. 
+• A mortgage calculator could collect values from a form, perform a 
+calculation, and display repayments. 
+• An animation could check the dimensions of the browser window 
+and move an image to the bottom of the viewable area (also known as 
+the viewport). 
+
+7. JS encompasses many of the traditional rules of programming. It can make the web page feel interacting by responding to what the user does.
+
+8. REACT TO EVENTS : You can specify that a script should run when a specific event has occurred. For example, it could be run when:
+• A button is pressed 
+• A link is clicked (or tapped) on 
+• A cursor hovers over an element 
+• Information is added to a form 
+• An interval of time has passed 
+• A web page has finished loading
+
+*/
+
+/* Before you learn how to read and write the JS language itself you need to before become familiar with some key concepts in computer programming.They will be covered in three sections:
+A: What is a Script and how do i crate one?
+B: How do computers fits in with the world arround them?
+C: How do I write a script for a web page?
+
+Note: Once you have learned the basics, you can start 
+*/
