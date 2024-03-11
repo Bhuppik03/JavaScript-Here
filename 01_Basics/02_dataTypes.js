a6a853f00e40ff9586ae4d822d50ab5819b0a2ab
Bhupendra Kumar <149204876+Bhuppik03@users.noreply.github.com>
2024-03-11 16:38:34 +0000

    Intro and data types done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treats all JS code as newer version or newer JS
+
+//alert( 3 + 3 ) //we are using node.js, not browser
+console.log(4
+    +
+    5) //Code readability shuold be high
+
+console.log(3+3);
+console.log("Bhuppi Here");
+
+let name = "Bhuppi Here" //string type data type
+let age = 21 //number types data type
+let isLoggedIn = false //Boolean data types
+
+/*  Data types ----> Preemptive type of data types  */
+// number => 2 to power 53
+// bigint => Manage large number of data
+// string => ""
+// boolean => true/false
+// null => stand alone value {representation of empty value/ null is not undefined}
+// undefined => 
+// symbol => we talk to unique, its called symbol / use component
+
+// object
+
+console.log(typeof "Bhuppi Here");
+console.log(typeof name);
+console.log(typeof age );
+console.log(typeof null); // type of null is object
+console.log(typeof undefined); // type of undefined is undefined
